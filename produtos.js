77644ae9a1e5483f37f1a7dec27fc8f7930635a0
Adrian Kerber <adrianmathiaskerber@gmail.com>
2025-11-17 15:32:40 -0400

    Add initial products list to produtos.js

---
+++ produtos.js
@@ -0,0 +1,12 @@
+const products = [
+  { id: 1, name: "Soja Premium", price: 142.90 },
+  { id: 2, name: "Milho Safra Nova", price: 78.50 },
+  { id: 3, name: "Adubo NPK 20-05-20", price: 220.00 },
+  { id: 4, name: "Semente de Trigo", price: 95.30 },
+  { id: 5, name: "Glifosato 480 SL", price: 38.75 },
+  { id: 6, name: "Inoculante para Soja", price: 129.90 },
+  { id: 7, name: "Fertilizante Foliar", price: 67.40 },
+  { id: 8, name: "Herbicida Pós-Emergente", price: 84.20 },
+  { id: 9, name: "Semente de Milheto", price: 51.60 },
+  { id: 10, name: "Fungicida Sistêmico", price: 156.00 }
+];
